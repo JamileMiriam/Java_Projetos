c9c6dc6ba5f74f2b5566caf239b83ceda5ddcd10
JamileMiriam <jmiryam16@gmail.com>
2019-04-01 23:43:43 -0300

    projeto listar alunos

---
+++ ListarAluno/src/listarAluno/Javaj.java
@@ -0,0 +1,19 @@
+package listarAluno;
+
+import java.sql.Connection;
+
+public class Javaj {
+    public static void main(String[] args) throws Exception {
+         
+        System.out.println(JdbcUtil.getConnection());
+        Connection connection = JdbcUtil.getConnection();
+        Aluno a1 = new Aluno();
+       // a1.setNome("teste");
+        AlunoDAO alunoDAO = new AlunoDAO(connection);
+        alunoDAO.insert(a1);
+        System.out.println(alunoDAO.listar());
+        
+        //alunoDAO.update(a1);
+        }
+    
+}
